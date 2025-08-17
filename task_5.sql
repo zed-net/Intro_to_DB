fab2abcb105026c361cf106ee8a9f848983dc7e5
zed <zedekjacob@gmail.com>
2025-08-17 17:23:04 +0100

    task 5

---
+++ task_5.sql
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+
+def get_table_description(alx_book_store, books):
+    
+   try:
+        # Establish a connection to the MySQL database.
+        with connect(
+            host=host,
+            user=user,
+            password=password,
+            database= alx_book_store
+        ) as connection:
+            # Create a cursor object to execute SQL queries.
+            with connection.cursor() as cursor:
+            
+                query = """
+                INSERT INTO customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+                """
+                cursor.execute(query)
+    except Error as e:
+        # Print a user-friendly error message if the connection fails.
+        print(f"Error connecting to MySQL: {e}")
+       
+    finally:
+        # Ensure the cursor and connection are closed
+        if 'cnx' in locals() and cnx.is_connected():
+            cursor.close()
+            cnx.close()
+            
